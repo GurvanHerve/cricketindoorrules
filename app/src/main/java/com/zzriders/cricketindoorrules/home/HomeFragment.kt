58bfc445c3c93cf27091d5e910aa62e8894e30bb
GurvanHERVE <gurvan.herve@gmail.com>
2019-05-08 11:34:43 +0200

    add home view

---
+++ app/src/main/java/com/zzriders/cricketindoorrules/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.zzriders.cricketindoorrules.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zzriders.cricketindoorrules.R
+import kotlinx.android.synthetic.main.home_fragment.view.*
+
+class HomeFragment : BaseFragment() {
+
+    interface HomeListener {
+        fun onGameClicked()
+        fun onNewTypeGameClicked()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.home_new_type_game.setOnClickListener { homeListener()?.onNewTypeGameClicked() }
+    }
+
+    private fun homeListener() : HomeListener? {
+        return if (activity is HomeListener) activity as HomeListener else null
+    }
+}
