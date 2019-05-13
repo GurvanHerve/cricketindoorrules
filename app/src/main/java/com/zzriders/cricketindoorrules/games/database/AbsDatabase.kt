79b7fc87c627a429fbc29255c04cf78b6c0027c6
gurvan.herve <gurvan.herve@mazedia.fr>
2019-05-13 13:59:56 +0200

    rename db and add companion object for access

--- app/src/main/java/com/zzriders/cricketindoorrules/games/database/AbsDatabase.kt
+++ app/src/main/java/com/zzriders/cricketindoorrules/games/database/AbsDatabase.kt
@@ -1,7 +1,8 @@
 package com.zzriders.cricketindoorrules.games.database
 
+import android.content.Context
+import androidx.room.*
 import androidx.room.Database
-import androidx.room.RoomDatabase
 import com.zzriders.cricketindoorrules.games.database.model.Game
 import com.zzriders.cricketindoorrules.games.database.model.Overs
 import com.zzriders.cricketindoorrules.games.database.model.Rules
@@ -11,10 +12,22 @@ import com.zzriders.cricketindoorrules.games.database.repositories.OversReposito
 import com.zzriders.cricketindoorrules.games.database.repositories.RulesRepository
 import com.zzriders.cricketindoorrules.games.database.repositories.TeamRepository
 
-@Database(entities = [Game::class, Team::class, Rules::class, Overs::class], version = 1)
-abstract class Database : RoomDatabase() {
+@Database(entities = [Game::class, Team::class, Rules::class, Overs::class], version = 1, exportSchema = false)
+abstract class AbsDatabase : RoomDatabase() {
     abstract fun gameRepository(): GameRepository
     abstract fun teamRepository(): TeamRepository
     abstract fun rulesRepository(): RulesRepository
     abstract fun oversRepository(): OversRepository
+
+    companion object {
+        @Volatile private var INSTANCE: AbsDatabase? = null
+
+        fun database(context: Context): AbsDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, AbsDatabase::class.java, "app.db").build()
+    }
 }
