b063766e881253dc6e759a0bd7c3e23ee6ba8e1e
gurvan.herve <gurvan.herve@mazedia.fr>
2019-05-15 14:06:26 +0200

    Add some relations (not what I want but need to go on with the app)

--- app/src/main/java/com/zzriders/cricketindoorrules/games/database/model/Game.kt
+++ app/src/main/java/com/zzriders/cricketindoorrules/games/database/model/Game.kt
@@ -3,20 +3,31 @@ package com.zzriders.cricketindoorrules.games.database.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 import java.util.*
 
-@Entity(tableName="game"
-//        foreignKeys = [
-//            ForeignKey(
-//                entity = Overs::class,
-//                parentColumns = ["uid"],
-//                childColumns = ["overs_uid"])
-//            ForeignKey(entity = Team::class,
-//                parentColumns = ["uid"],
-//                childColumns = ["team_uid"])
-//    ]
-)
+@Entity(tableName="game",
+        foreignKeys = [
+            ForeignKey(
+                entity = Team::class,
+                parentColumns = ["uid"],
+                childColumns = ["team_two_uid"],
+                onDelete = CASCADE
+            ),
+            ForeignKey(
+                entity = Team::class,
+                parentColumns = ["uid"],
+                childColumns = ["team_one_uid"],
+                onDelete = CASCADE
+            ),
+            ForeignKey(
+                entity = Overs::class,
+                parentColumns = ["uid"],
+                childColumns = ["overs_uid"],
+                onDelete = CASCADE
+            )
+])
 class Game {
     @PrimaryKey
     var uid = UUID.randomUUID().toString()
@@ -24,10 +35,12 @@ class Game {
     @ColumnInfo(name="name")
     var name: String = ""
 
-//    @ColumnInfo(name="team_uid")
-//    val team: String = ""
-//    val team = mutableListOf<Team>()
+    @ColumnInfo(name="team_one_uid")
+    var teamOneUid = ""
+
+    @ColumnInfo(name="team_two_uid")
+    var teamTwoUid = ""
 
     @ColumnInfo(name="overs_uid")
-    var overs: String = ""
+    var oversUid = ""
 }
