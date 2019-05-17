9ee28ae49efec02bbe288fd7b8befeb7c496738e
gurvan.herve <gurvan.herve@mazedia.fr>
2019-05-17 14:10:31 +0200

    interact fragment with presenter

--- app/src/main/java/com/zzriders/cricketindoorrules/games/presenters/PlayersPresenter.kt
+++ app/src/main/java/com/zzriders/cricketindoorrules/games/presenters/PlayersPresenter.kt
@@ -1,23 +1,50 @@
 package com.zzriders.cricketindoorrules.games.presenters
 
+import com.zzriders.cricketindoorrules.games.database.model.Team
 import com.zzriders.cricketindoorrules.games.views.PlayersView
 
 class PlayersPresenter(val view: PlayersView) {
 
-    private val team = Team()
+    private val teamOne = Team()
+    private val teamTwo = Team()
 
-    fun setTeamName(name: String) {
-        team.name = name
+    fun incrementPlayersCountTeamOne() {
+        if (teamOne.playersCount == 0) view.disableDecrementPlayerTeamOne(false)
+        teamOne.playersCount = teamOne.playersCount + 1
+        view.setPlayerCountTeamOne(teamOne.playersCount.toString())
+        validateAndEnableOk()
     }
 
-    fun incrementPlayersCount() {
-        if (team.playerCount == 0) view.disableDecrementPlayer(false)
-        team.playerCount = team.playerCount + 1
+    fun decrementPlayersCountTeamOne() {
+        teamOne.playersCount = teamOne.playersCount - 1
+        view.setPlayerCountTeamOne(teamOne.playersCount.toString())
+        if (teamOne.playersCount == 0) view.disableDecrementPlayerTeamOne(true)
+        validateAndEnableOk()
     }
 
-    fun decrementPlayersCount() {
-        team.playerCount = team.playerCount - 1
+    fun incrementPlayersCountTeamTwo() {
+        if (teamTwo.playersCount == 0) view.disableDecrementPlayerTeamTwo(false)
+        teamTwo.playersCount = teamTwo.playersCount + 1
+        view.setPlayerCountTeamTwo(teamTwo.playersCount.toString())
+        validateAndEnableOk()
+    }
 
-        if (team.playerCount == 0) view.disableDecrementPlayer(true)
+    fun decrementPlayersCountTeamTwo() {
+        teamTwo.playersCount = teamTwo.playersCount - 1
+        view.setPlayerCountTeamTwo(teamTwo.playersCount.toString())
+        if (teamTwo.playersCount == 0) view.disableDecrementPlayerTeamTwo(true)
+        validateAndEnableOk()
+    }
+
+    private fun validateAndEnableOk() {
+        view.enableOk(teamOne.playersCount > 0 && teamTwo.playersCount > 0)
+    }
+
+    fun teamOne() : Team {
+        return teamOne
+    }
+
+    fun teamTwo() : Team {
+        return teamTwo
     }
 }
--- app/src/main/java/com/zzriders/cricketindoorrules/games/views/PlayersView.kt
+++ app/src/main/java/com/zzriders/cricketindoorrules/games/views/PlayersView.kt
@@ -1,5 +1,9 @@
 package com.zzriders.cricketindoorrules.games.views
 
 interface PlayersView {
-    fun disableDecrementPlayer(disable: Boolean)
+    fun disableDecrementPlayerTeamOne(disable: Boolean)
+    fun disableDecrementPlayerTeamTwo(disable: Boolean)
+    fun setPlayerCountTeamOne(value: String)
+    fun setPlayerCountTeamTwo(value: String)
+    fun enableOk(enable: Boolean)
 }
